c7b7865ba63712a60c7341fc6412e4038c401dbd
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-04-06 00:00:27 +0800

    Added events

--- src/client.js
+++ src/client.js
@@ -1,13 +1,24 @@
-const WebSocket = require("ws");
+const WebSocket = require("ws"),
+      events = require("events"),
+      axiosRequire = require("axios").default;
 
 class Bot {
     /**
      * Create a new Bot object
+     * @param {string} appId  Application ID of the bot
      * @param {string} token  Token of the bot
      */
-    constructor(token) {
+    constructor(appId, token) {
         this.token = token;
+        this.appId = appId;
         this.ws = null; // Will be filled after login() is called
+        this.events = new events.EventEmitter();
+        this.axios = axiosRequire.create({
+            baseURL: "https://discord.com/api/v8",
+            headers: {
+                authoization: `Bot ${this.token}`
+            }
+        });
     }
 
     /**
@@ -21,9 +32,9 @@ class Bot {
 
         let ack, hb, heartbeat;
         this.ws.on("message", payload => {
-            const data = JSON.parse(payload);
+            const {op, d:data, s:seq, t:event } = JSON.parse(payload);
 
-            console.log(data.op);
+            console.log(op);
 
             const beat = () => {
                 if (ack === false) {
@@ -44,9 +55,36 @@ class Bot {
                 console.log("Heartbeated");
             };
 
-            switch (data.op) {
+            switch (op) {
                 case 0: {
-                    
+                    console.log(event);
+                    // If the event is a slash command, event would be /<command> like /ping, otherwise the regular event name will be used
+                    if (event === "INTERACTION_CREATE") {
+                        this.events.emit(event, JSON.parse((data === null) ? data : "{}"));
+                    } else {
+                        const slashData = JSON.parse(data);
+                        if (this.respond) {
+                            this.axios.post(`/interactions/${slashData.interaction.id}/${slashData.interaction.token}/callback`, {
+                                type: 5
+                            });
+                        }
+                        this.events.emit(`/${slashData.data.name}`, {
+                            /**
+                             * Make a message response to the user. You can use either message and/or embed, you must pick one though. Leave message as "" if you wish to leave it blank.
+                             * @param {string} message Plain text (markdown accepted) reply
+                             * @param {string} embedData Embed reply, please see https://discord.com/developers/docs/resources/channel#embed-object for proper formatting
+                             */
+                            reply: function(message, embedData) {
+                                this.axios.patch(`/webhooks/${this.appId}/${slashData.interaction.id}/messages/@original`);
+                            },
+                            data: {
+                                type: slashData.type,
+                                guild: slashData.guild_id,
+                                channel: slashData.channel_id,
+                                options: slashData.data.options
+                            }
+                        });
+                    }
                     break;
                 }
                 case 1: {
@@ -61,7 +99,7 @@ class Bot {
                     heartbeat = setInterval(() => {
                         beat();
                         ack = false;
-                    }, data.d.heartbeat_interval);
+                    }, data.heartbeat_interval);
 
                     // identify
                     this.ws.send(JSON.stringify({
@@ -80,7 +118,7 @@ class Bot {
                 }
                 case 11: {
                     ack = true;
-                    hb = (data.d == null) ? null : data.d.s;
+                    hb = (data == null) ? null : data.s;
                     break;
                 }
             }
